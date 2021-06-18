9a5dbf57b3b0fad34ea39cae4341393d87b6457a
Kevin Katz <kevcmk@gmail.com>
2021-06-18 13:32:49 -0700

    Animation, flipped, fxnl

--- tests/test_gravity.py
+++ tests/test_gravity.py
@@ -10,8 +10,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from rgb import gravity
 
 hz = 30
-dt = 1/30
-g = gravity.Gravity(64, 32, 0.320, 0.160, 30, 256)
+dt = 1/hz
+g = gravity.Gravity(64, 32, 0.320, 0.160, 30, 128)
 #img = g.step(dt)
 
 
@@ -22,7 +22,7 @@ canvas.pack()
 
 while True:
     img = g.step(dt)
-    img_larger = img.resize((img.width * 4, img.height * 4),resample=0)
+    img_larger = img.resize((img.width * 4, img.height * 4),resample=0).transpose(Image.FLIP_TOP_BOTTOM)
     # img = Image.open("/Users/katz/Pictures/unnamed_b.png")
     # If this image is not here, it will be garbage collected (and will not appear)
     pi = ImageTk.PhotoImage(img_larger)
