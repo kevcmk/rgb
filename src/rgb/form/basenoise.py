9850c4189aaec827a827675ec47f8db9f00cda3f
Kevin Katz <kevcmk@gmail.com>
2021-07-25 21:42:59 -0600

    Keys rendering as noisemask

--- src/rgb/form/basenoise.py
+++ src/rgb/form/basenoise.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-from rgb.utilities import hsv_to_pixel
+
 import logging
-import math
 import os
 import time
 from dataclasses import dataclass
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 
 import numpy as np
+from noise import pnoise3, snoise3
 from PIL import Image
+from rgb.constants import NUM_NOTES
 from rgb.form.baseform import BaseForm
-from rgb.utilities import clamp
-from noise import snoise3, pnoise3
+from rgb.utilities import hsv_to_pixel
+
+from form.keys import KeyAwareForm
+
 log = logging.getLogger(__name__)
 log.setLevel(level=os.environ.get("PYTHON_LOG_LEVEL", "INFO"))
 
@@ -39,42 +42,7 @@ class BaseNoise(BaseForm):
         
         self.t_start = time.time()
         
-    def cleanup(self):
-        self.presses = dict()
-
-    
-    def midi_handler(self, value: Dict):
-        # Key Press: msg.dict() -> {'type': 'note_on', 'time': 0, 'note': 48, 'velocity': 127, 'channel': 0} {'type': 'note_off', 'time': 0, 'note': 48, 'velocity': 127, 'channel': 0}
-        
-        if value['type'] == 'note_on':
-            note = value['note']
-            velocity = value['velocity'] / MIDI_DIAL_MAX
-            self.presses[note] = Press(t=time.time(), 
-            note=note, 
-            velocity=velocity)
-        elif value['type'] == 'note_off':
-            note = value['note']
-            if note in self.presses:
-                del self.presses[note]
-        elif value['type'] == 'control_change' and value['control'] == 14: 
-            self.wave_speed = 2 * value['value']
-        elif value['type'] == 'control_change' and value['control'] == 15:
-            self.wave_step = int(value['value'] / 4) + 1
-        elif value['type'] == 'control_change' and value['control'] == 16:
-            self.wave_width = min(
-                int(math.log(value['value'] + 1)), 
-                self.wave_step
-            )
-        elif value['type'] == 'control_change' and value['control'] == 16:
-            self.wave_width = min(
-                int(math.log(value['value'] + 1)), 
-                self.wave_step
-            )
-    
-        else:
-            log.debug(f"Unhandled message: {value}")
-
-    def noisexy(self, x, y, z) -> float:
+    def select_noise(self, x, y, z) -> float:
         return self.NOISE_FUNCTIONS[self.noise_function_index](y * self.scale, x * self.scale, z * self.timescale, octaves=self.octaves, persistence=self.persistence)
 
     def noise_to_pixel(self, v: float):
@@ -87,7 +55,7 @@ class BaseNoise(BaseForm):
         dt = time.time() - self.t_start
         for i in range(self.matrix_height):
             for j in range(self.matrix_width):
-                v = self.noisexy(x=j, y=i, z=dt)
+                v = self.select_noise(x=j, y=i, z=dt)
                 res[i,j,:] = self.noise_to_pixel(v)
         
         return Image.fromarray(res) #.transpose(Image.FLIP_TOP_BOTTOM)
@@ -113,3 +81,27 @@ class HueNoise(BaseNoise):
     def noise_to_pixel(self, v: float):
         return hsv_to_pixel((v + 1.0) / 2, 1.0, 1.0)
 
+
+class NoiseKey(BaseNoise, KeyAwareForm):
+        
+    def step(self, dt) -> Union[Image.Image, np.ndarray]:
+        time_elapsed = time.time() - self.t_start
+        notespace = np.zeros((len(self.presses), self.matrix_height, self.matrix_width, 3), dtype=np.uint8)
+        xs = np.linspace(start=0, stop=self.matrix_width, num=NUM_NOTES + 1, endpoint=True, dtype=np.uint8)
+        for index, v in enumerate(self.presses.values()):
+            note_index = v.note % NUM_NOTES
+            hue = note_index / NUM_NOTES
+            lo = xs[note_index]
+            hi = xs[note_index + 1] 
+            for i in range(self.matrix_height):
+                for j in range(lo, hi):
+                    noise_value = self.select_noise(x=j, y=i, z=time_elapsed)
+                    pixel = self.noise_to_pixel(noise_value) 
+                    notespace[index, i, j, 0] = pixel[0]
+                    notespace[index, i, j, 1] = pixel[1]
+                    notespace[index, i, j, 2] = pixel[2] 
+
+        res = np.mean(notespace, axis=0, dtype=np.uint8)
+
+
+        return res
