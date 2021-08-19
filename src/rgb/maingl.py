6a4deba7c99ba575eabbc76c4da2e91e41f58f47
Kevin Katz <kevcmk@gmail.com>
2021-08-18 17:13:50 -0700

    Gltriangle

--- src/rgb/maingl.py
+++ src/rgb/maingl.py
@@ -1,15 +1,22 @@
-import pyglet
+from pyglet.gl import *
 
+# Direct OpenGL commands to this window.
 window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+
 
 @window.event
 def on_draw():
-    window.clear()
-    label.draw()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glBegin(GL_TRIANGLES)
+    glVertex2f(0, 0)
+    glVertex2f(window.width, 0)
+    glVertex2f(window.width, window.height)
+    glEnd()
 
-pyglet.app.run()
+if __name__ == "__main__":
+    context = window.context
+    config = context.config
+    config.double_buffer
+    import ipdb; ipdb.set_trace()
+    pyglet.app.run()
